c17dd57eb907ebe46d6656be19793fdd691a0659
정상필의GRAM\mod45 <mod459@naver.com>
2024-08-09 16:30:30 +0900

    충돌 해결

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,10 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import { Provider, useDispatch } from 'react-redux';
 import Login from './pages/user/login';
 import Signup from './pages/user/signup';
-<<<<<<< HEAD
-=======
 import Home from './pages/home';
 import Diary from './pages/diary/diary';
 import DiaryAdd from './pages/diary/diaryAdd';
->>>>>>> origin/main
+
 
 import PrivateRoute from './component/privateRoute';
 import { clearUser, initializeUser } from './store/authSlice';
