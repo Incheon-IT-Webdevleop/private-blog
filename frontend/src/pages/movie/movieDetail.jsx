3d713c4f2296eda1f13ed14d3f3938169c109ed1
leeuihyoun <dasuu3@naver.com>
2024-08-16 13:51:38 +0900

    카테고리 안올라가는 버그 수정

--- frontend/src/pages/movie/movieDetail.jsx
+++ frontend/src/pages/movie/movieDetail.jsx
@@ -4,6 +4,16 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 import parse from 'html-react-parser';  // html-react-parser 임포트
 import '../movie/movieDetail.css';
+const genres = {
+    1: '액션',
+    2: '코미디',
+    3: '드라마',
+    4: '호러',
+    5: 'SF',
+    6: '판타지',
+    7: '로맨스',
+    8: '스릴러'
+  };
 
 export default function MovieDetail() {
     const { id } = useParams();  // URL에서 id 추출
@@ -31,11 +41,12 @@ export default function MovieDetail() {
     if (!review) {
         return <p>리뷰를 불러오는 중입니다...</p>;  // 리뷰 데이터가 없을 때 로딩 메시지
     }
-
+    const genreName = genres[review.reviewCategory] || '없음';
     return (
         <div className="review-detail-container">
             <h1>{review.reviewTitle}</h1>
             <p>{new Date(review.reviewDate).toLocaleDateString()}</p>
+            <p>장르: {genreName}</p>
             <div className="review-content">
                 {parse(review.reviewContent)} 
             </div>
